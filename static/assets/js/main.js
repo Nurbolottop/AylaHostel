e4b6c2ad6c82ba155453a12f20aac11aa187614a
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-02-11 03:08:40 -0500

    Где мы есть ?

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -512,22 +512,22 @@
             Mouse Cursor Js
         ========================================
         */
-        var myCursor = $('.mouse-move');
-        if (myCursor.length) {
-            if ($('body')) {
-                const e = document.querySelector('.mouse-inner'),
-                    t = document.querySelector('.mouse-outer');
-                let n, i = 0,
-                    o = !1;
-                window.onmousemove = function(s) {
-                    o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
-                }, $('body').on("mouseenter", "a, .cursor-pointer", function() {
-                    e.classList.add('mouse-hover'), t.classList.add('mouse-hover')
-                }), $('body').on("mouseleave", "a, .cursor-pointer", function() {
-                    $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove('mouse-hover'), t.classList.remove('mouse-hover'))
-                }), e.style.visibility = "visible", t.style.visibility = "visible"
-            }
-        }
+        // var myCursor = $('.mouse-move');
+        // if (myCursor.length) {
+        //     if ($('body')) {
+        //         const e = document.querySelector('.mouse-inner'),
+        //             t = document.querySelector('.mouse-outer');
+        //         let n, i = 0,
+        //             o = !1;
+        //         window.onmousemove = function(s) {
+        //             o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+        //         }, $('body').on("mouseenter", "a, .cursor-pointer", function() {
+        //             e.classList.add('mouse-hover'), t.classList.add('mouse-hover')
+        //         }), $('body').on("mouseleave", "a, .cursor-pointer", function() {
+        //             $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove('mouse-hover'), t.classList.remove('mouse-hover'))
+        //         }), e.style.visibility = "visible", t.style.visibility = "visible"
+        //     }
+        // }
 
         /* 
         ========================================
